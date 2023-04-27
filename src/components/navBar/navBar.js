5577e89b268a5c6adc4c0ee1223b57ad6d1d0696
Amen <jamen.jamne30@gmail.com>
2023-04-27 12:17:37 +0300

    sidebar finished

--- src/components/navBar/navBar.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function navBar() {
-  return (
-    <div>navBar</div>
-  )
-}
---
+++ src/components/navBar/sideBar.js
@@ -0,0 +1,106 @@
+import "./sideBar.css";
+import { useState } from "react";
+import { NavLink } from "react-router-dom";
+import {
+  BiHomeAlt,
+  BiLogOut,
+  BiChevronRight,
+  BiBarChartSquare,
+  BiGroup,
+  BiBriefcase,
+  BiFile,
+} from "react-icons/bi";
+import { MdOutlinePersonPin } from "react-icons/md";
+import logo from "./logo.svg";
+
+const Sidebar = (props) => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const handleToggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+  };
+
+  return (
+    <>
+      <div className={`sidebar ${isSidebarOpen ? "close" : ""}`}>
+        <div className="head">
+          <div className="image-text">
+            <span className="image">
+              <img src={logo} alt="logo" />
+            </span>
+
+            <div className="text logo-text">
+              <span className="name">Welcome</span>
+              <span className="profession">CEO</span>
+            </div>
+          </div>
+          <BiChevronRight className="toggle" onClick={handleToggleSidebar} />
+        </div>
+
+        <div className="menu-bar">
+          <div className="menu">
+            <ul className="menu-links">
+              <li className="nav-link">
+                <NavLink to="/">
+                  <BiHomeAlt className="icon" />
+                  <span className="text nav-text">anything</span>
+                </NavLink>
+              </li>
+
+              <li className="nav-link">
+                <NavLink to="/test">
+                  <BiHomeAlt className="icon" />
+                  <span className="text nav-text">anything</span>
+                </NavLink>
+              </li>
+
+              <li className="nav-link">
+                <NavLink to="/test">
+                  <BiHomeAlt className="icon" />
+                  <span className="text nav-text">anything</span>
+                </NavLink>
+              </li>
+
+              <li className="nav-link">
+                <NavLink to="/test">
+                  <BiHomeAlt className="icon" />
+                  <span className="text nav-text">anything</span>
+                </NavLink>
+              </li>
+
+              <li className="nav-link">
+                <NavLink to="/test">
+                  <BiHomeAlt className="icon" />
+                  <span className="text nav-text">anything</span>
+                </NavLink>
+              </li>
+
+              <li className="nav-link">
+                <NavLink to="/test">
+                  <BiHomeAlt className="icon" />
+                  <span className="text nav-text">anything</span>
+                </NavLink>
+              </li>
+            </ul>
+          </div>
+
+          <div className="bottom-content">
+            <li className="nav-link">
+              <NavLink to="/test">
+                <BiHomeAlt className="icon" />
+                <span className="text nav-text">anything</span>
+              </NavLink>
+            </li>
+            <li className="">
+              <NavLink to="/test">
+                <BiHomeAlt className="icon" />
+                <span className="text nav-text">anything</span>
+              </NavLink>
+            </li>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Sidebar;
