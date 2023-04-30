366f89574b70e714b8340765b07212f7867f78ef
Marwa <marwa.al.shahal@gmail.com>
2023-05-01 00:40:00 +0300

    routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
-import './App.css';
-import Founders from './pages/founders/Founders';
-import Home from './pages/home/home.js';
-import Profile from './pages/profile/Profile';
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
 
+import Founders from "./pages/founders/Founders";
+import Home from "./pages/home/home.js";
+import Profile from "./pages/profile/Profile";
+import Events from "./pages/events/Events";
+import Opportunities from "./pages/opportunities/Opportunities";
+import ContactUs from "./pages/contactUs/ContactUs";
+import Founders from "./pages/founders/Founders";
+import Volunteers from "./pages/volunteers/Volunteers";
+import Team from "./pages/team/Team";
 
 function App() {
   return (
     <div className="App">
-     
+      <div className="App-container">
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route exact path="/home" element={<Home />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/volunteers" element={<Volunteers />} />
+          <Route path="/team" element={<Team />} />
+          <Route path="/founders" element={<Founders />} />
+          <Route path="/events" element={<Events />} />
+          <Route path="/opportunities" element={<Opportunities />} />
+          <Route path="/contactUs" element={<ContactUs />} />
+
+          {/* <Route element={<PrivateSuperAdmin />}>
+              <Route path="/admin" element={<Admin />} />
+            </Route> */}
+        </Routes>
+      </div>
     </div>
   );
 }
